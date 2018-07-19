f2bbdfaf103bbeef64d8b1176cbaccabf8772e82
Thet Win Aung <minthit21dj@gmail.com>
2018-07-19 23:40:48 +0900

    Add diff sample

--- ridgepole/command.sh
+++ ridgepole/command.sh
@@ -8,3 +8,9 @@ ridgepole -c database.yml -E development --apply -f Schemafile
 # export
 ridgepole -c database.yml -E development --export --output schemas/Schemafile
 ridgepole -c database.yml -E development --export --split --output schemas/Schemafile
+
+# diff
+ridgepole -E development --diff database.yml schemas/Schemafile
+
+# diff by db each config
+ridgepole --diff production.yml development.yml
